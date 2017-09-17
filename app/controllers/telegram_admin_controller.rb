b8b1247eea4461ee50c8deaf65eb58e8adf92fb8
ggoha <ryznikov@mail.ru>
2017-09-18 00:24:06 +0300

    snall fix

--- app/controllers/telegram_admin_controller.rb
+++ app/controllers/telegram_admin_controller.rb
@@ -12,7 +12,7 @@ class TelegramAdminController < Telegram::Bot::UpdatesController
   end
 
   def summary
-    if message['chat']['id']==Rails.application.secrets['telegram']['admin']
+    if update['message']['chat']['id']==Rails.application.secrets['telegram']['admin']
       respond_with :message, text: summary_report, parse_mode: 'Markdown'
     else
       bot.send_message chat_id: Rails.application.secrets['telegram']['me'], text: 'неправильный канал'
@@ -34,13 +34,15 @@ class TelegramAdminController < Telegram::Bot::UpdatesController
         next if arr.empty?
         result_str << "На #{company.title} пошло #{arr.count} человек"
         comrads_percentage = arr.average(:buff)
-        result_str << " вместе с #{comrads_percentage.round(0)} %" if comrads_percentage
+        result_str << " вместе с #{comrads_percentage.round(0)}%. " if comrads_percentage
         sum_score = arr.sum(:score)
         result_str << "Они принесли #{sum_score}🏆 (#{(sum_score.to_f / battle.score * 100).round(2) }%)\n"
       end
       sum_score = reports.pluck(:score).inject(0, :+)
       result_str << "Отряд заработал #{sum_score}🏆 (#{(sum_score.to_f / battle.score * 100).round(2) }%)\n\n"
     end
+    sum_score = battle.reports.pluck(:score).inject(0, :+)
+    result_str << "\nВсего обработано #{sum_score}🏆 (#{(sum_score.to_f / battle.score * 100).round(2) }%)"
     result_str
   end
 end
